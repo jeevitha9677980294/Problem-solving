3b7beae21abca7c984b72c3eed4b59721947020c
Jeevitha S <your_email@example.com>
2026-09-07 15:22:12 +0530

    reverse a number

--- reverse_number/rev.java
+++ reverse_number/rev.java
@@ -8,7 +8,7 @@ public class rev {
       int digit = N % 10;
       // formula
       reverse = reverse * 10 + digit;
-      N = N / 10;
+      N = N / 10;   
     }
     System.out.println(reverse);
   }
